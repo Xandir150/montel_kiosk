72f79a04d5cb6cacb99184b946874f4075186ac2
Alexander Shekhovtsov <16119449+Xandir150@users.noreply.github.com>
2020-07-16 10:10:33 +0200

    browser run

---
+++ kiosk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/user/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/'   /home/user/.config/chromium/Default/Preferences
+
+/usr/bin/chromium --disable-application-cache --media-cache-size=1 --disk-cache-size=1 --noerrdialogs --disable-infobars --kiosk --disable-web-security --user-data-dir=/home/user/.config/chromium/ /home/user/LocalPublish/index.html &
+
+while true; do
+   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+   sleep 10
+done
